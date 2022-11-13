e4255a4e8f2f36c0ea8add605a549169b92ef72a
L-Pig <1215884602@qq.com>
2022-11-13 18:09:18 +0800

    tvx address change f4 address

--- test_tvx/tests/exec.rs
+++ test_tvx/tests/exec.rs
@@ -1,18 +1,20 @@
 mod util;
 
 use std::{env, fs};
+use fil_actor_eam as eam;
 use fvm_shared::address::Address;
-use test_tvx::{EVM_ACTOR_CODE_ID, EvmContractInput, is_create, string_to_bytes, string_to_U256};
+use test_tvx::{EVM_ACTOR_CODE_ID, EvmContractInput, is_create, string_to_bytes, string_to_ETHAddress, string_to_U256};
 
 #[test]
 fn exec_contract_construction_and_invocation() {
     let input_evm_contract_data = env::var("INPUT_EVM_CONTRACT_DATA").expect("Please set the environment variable (env: INPUT_EVM_CONTRACT_DATA) ");
     let input_evm_contract_content = fs::read_to_string(input_evm_contract_data).unwrap();
     let input: EvmContractInput = serde_json::from_str(&input_evm_contract_content).unwrap();
-    //todo actor_id和evm contract address 映射问题
-    let contract = Address::new_id(100);
+    let id_addr = Address::new_id(10);
+    let eth_addr = eam::EthAddress(string_to_ETHAddress(input.context.from.clone()).0);
+    let contract = Address::new_delegated(id_addr.id().unwrap(), &eth_addr.0).unwrap();
     let mut rt = util::init_construct_and_verify(&input, |rt| {
-        rt.actor_code_cids.insert(contract, *EVM_ACTOR_CODE_ID);
+        rt.actor_code_cids.insert(id_addr, *EVM_ACTOR_CODE_ID);
         rt.set_origin(contract);
     });
 
--- test_tvx/tests/util.rs
+++ test_tvx/tests/util.rs
@@ -8,14 +8,6 @@ use fil_actor_evm::interpreter::address::EthAddress;
 use fil_actors_runtime::runtime::Runtime;
 use test_tvx::{EvmContractInput, INIT_ACTOR_CODE_ID, is_create, MockRuntime, string_to_bytes, string_to_ETHAddress, string_to_U256};
 
-// #[allow(dead_code)]
-// pub fn construct_and_verify(initcode: Vec<u8>) -> MockRuntime {
-//     init_construct_and_verify(initcode, |_| {})
-// }
-
-pub const CONTRACT_ADDRESS: [u8; 20] =
-    hex_literal::hex!("FEEDFACECAFEBEEF000000000000000000000000");
-
 pub fn init_construct_and_verify<F: FnOnce(&mut MockRuntime)>(
     input: &EvmContractInput,
     initrt: F,
@@ -30,7 +22,7 @@ pub fn init_construct_and_verify<F: FnOnce(&mut MockRuntime)>(
     // first actor created is 0
     rt.add_delegated_address(
         Address::new_id(0),
-        Address::new_delegated(EAM_ACTOR_ID, &CONTRACT_ADDRESS).unwrap(),
+        Address::new_delegated(EAM_ACTOR_ID, &string_to_ETHAddress(input.context.to.clone()).0).unwrap(),
     );
 
     if is_create(input.context.to.clone()) {
@@ -52,58 +44,6 @@ pub fn init_construct_and_verify<F: FnOnce(&mut MockRuntime)>(
         let res = hex::encode(bytecode.to_vec());
         rt.set_return_result(res);
     } else {
-
-    }
-
-    rt.verify();
-
-    rt
-}
-
-pub fn init_construct_and_verify2<F: FnOnce(&mut MockRuntime)>(
-    input: &EvmContractInput,
-    initrt: F,
-) -> MockRuntime {
-    let mut rt = MockRuntime::default();
-
-    // construct EVM actor
-    rt.set_caller(*INIT_ACTOR_CODE_ID, INIT_ACTOR_ADDR);
-    rt.expect_validate_caller_type(vec![Type::Init]);
-    initrt(&mut rt);
-
-    // first actor created is 0
-    rt.add_delegated_address(
-        Address::new_id(0),
-        Address::new_delegated(EAM_ACTOR_ID, &CONTRACT_ADDRESS).unwrap(),
-    );
-
-    if is_create(input.context.to.clone()) {
-        let mut system = System::create(&mut rt).unwrap();
-        for state in input.states.clone() {
-            for e in state.partial_storage_before {
-                let k = string_to_U256(e.0.clone());
-                let v = string_to_U256(e.1.clone());
-                system.set_storage(k, Some(v)).unwrap();
-            }
-        }
-        let initcode = string_to_bytes(input.context.input.clone());
-        let params = evm::ConstructorParams {
-            creator: string_to_ETHAddress(input.context.from.clone()),
-            initcode: initcode.into(),
-        };
-
-        assert!(rt
-            .call::<evm::EvmContractActor>(
-                evm::Method::Constructor as u64,
-                &RawBytes::serialize(params).unwrap(),
-            )
-            .unwrap()
-            .is_empty());
-        // let system = System::load(&mut rt, true).unwrap();
-        // let bytecode = system.load_bytecode().unwrap().unwrap();
-        // let res = hex::encode(bytecode.to_vec());
-        // rt.set_return_result(res);
-    } else {
         rt.in_call = true;
         rt.validate_immediate_caller_type(iter::once(&Type::Init)).unwrap();
         let mut system = System::create(&mut rt).unwrap();
@@ -119,6 +59,7 @@ pub fn init_construct_and_verify2<F: FnOnce(&mut MockRuntime)>(
         system.flush().unwrap();
         rt.in_call = false;
     }
+
     rt.verify();
 
     rt
